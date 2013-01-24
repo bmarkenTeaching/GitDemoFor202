bf49b72c877e6ba21882fe7e06a1f3e46040c22b
Brandon Marken <brandon.marken@gmail.com>
2013-01-24 10:03:01 -0900

    This is a stupid program and we are lazily adding using namespace std

--- GitDemoFor202/main.cpp
+++ GitDemoFor202/main.cpp
@@ -7,12 +7,13 @@
 //
 
 #include <iostream>
+using namespace std;        //because i am a lazy person
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return 0;
 }
 
